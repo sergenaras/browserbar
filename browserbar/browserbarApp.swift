6627b03ef624e62ec5ae9b305876a02739064627
Sergen Aras <srgnras@gmail.com>
2025-12-10 20:07:07 +0300

    Initial Commit

---
+++ browserbar/browserbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  browserbarApp.swift
+//  browserbar
+//
+//  Created by Sergen on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct browserbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
